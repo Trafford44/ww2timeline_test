af61a5b63f8f07ca9a0cd372c84248b69ce9f148
Trafford44 <nzrs@outlook.com>
2025-10-18 12:32:20 +1300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -16,6 +16,7 @@ export async function fetchAndRenderData(features, domain, settings) {
       throw new Error(`No dataset mapped for domain: ${subject}`);
     }
     const datasetURL = datasetMap[subject];
+    //fetch is a built-in browser API
     const response = await fetch(datasetURL);
     const data = await response.json();
     console.log("📦 Loaded dataset:", data);
