69c28b1d98bc44555bf83dc87d2061db687c4356
Trafford44 <nzrs@outlook.com>
2025-10-23 08:29:44 +1300

    Update domain.js

--- js/domain.js
+++ js/domain.js
@@ -1,1 +1,6 @@
+// get access to the domain key from any module by adding
+//    import { domainKey } from './domain.js';
+//    const config = await loadConfig(domainKey);
+
+// Use a dynamic key based on URL, user input, or fallback
 export const domainKey = new URLSearchParams(window.location.search).get("domain") || "ww2infilm";
